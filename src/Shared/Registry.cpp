5496ef319162f25f88190e6049eac4fadf2e0a3e
Mifan Bang <mifanbang@gmail.com>
2026-08-16 00:48:48 +0800

    Added Windows registry helper to the shared library

---
+++ src/Shared/Registry.cpp
@@ -0,0 +1,68 @@
+/*
+ *  Lauss - PoC blocking ad banners in LINE clients on Windows
+ *  Copyright (C) 2023-2026 Mifan Bang <https://debug.tw>.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "Registry.h"
+
+#include <Types.h>
+
+#include <windows.h>
+
+#include <cassert>
+#include <string_view>
+
+
+
+RegistryKey::RegistryKey(std::wstring_view name)
+	: m_hKey(nullptr)
+{
+	constexpr gan::WinDword k_reserved = 0;
+	constexpr wchar_t* k_noUserClass = nullptr;
+	constexpr LPSECURITY_ATTRIBUTES k_noSecAttr = nullptr;
+	constexpr gan::WinDword* k_noDispotionInfo = nullptr;
+	::RegCreateKeyExW(
+		HKEY_CURRENT_USER,
+		name.data(),
+		k_reserved,
+		k_noUserClass,
+		REG_OPTION_NON_VOLATILE,
+		KEY_READ | KEY_WRITE,
+		k_noSecAttr,
+		&m_hKey,
+		k_noDispotionInfo
+	);
+}
+
+RegistryKey::~RegistryKey()
+{
+	if (operator bool())
+		::RegCloseKey(m_hKey);
+}
+
+bool RegistryKey::SetValueUnsafe(std::wstring_view name, Type type, const void* value, size_t size)
+{
+	constexpr wchar_t* k_noSubKey = nullptr;
+	const auto setKeyResult = ::RegSetKeyValueW(
+		m_hKey,
+		k_noSubKey,
+		name.data(),
+		std::to_underlying<Type>(type),
+		value,
+		static_cast<gan::WinDword>(size)
+	);
+	return setKeyResult == NO_ERROR;
+}
---
+++ src/Shared/Registry.h
@@ -0,0 +1,67 @@
+/*
+ *  Lauss - PoC blocking ad banners in LINE clients on Windows
+ *  Copyright (C) 2023-2026 Mifan Bang <https://debug.tw>.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <Types.h>
+
+#include <cassert>
+#include <string_view>
+
+
+// Forward declarations
+struct HKEY__;
+using HKEY = HKEY__*;
+
+
+class RegistryKey
+{
+public:
+	enum class Type : gan::WinDword
+	{
+		String = 1
+	};
+
+	RegistryKey(std::wstring_view name);
+	~RegistryKey();
+
+	RegistryKey(const RegistryKey&) = delete;
+	RegistryKey(RegistryKey&&) = delete;
+	RegistryKey& operator=(const RegistryKey&) = delete;
+	RegistryKey& operator=(RegistryKey&&) = delete;
+
+	template <Type type>
+		requires (type == Type::String)
+	bool SetValue(std::wstring_view name, const wchar_t* value, size_t size)
+	{
+		assert(operator bool());
+		if (!operator bool())
+			return false;
+		return SetValueUnsafe(name, type, value, size);
+	}
+
+	operator bool() const
+	{
+		return m_hKey != nullptr;
+	}
+
+private:
+	bool SetValueUnsafe(std::wstring_view name, Type type, const void* value, size_t size);
+
+	HKEY m_hKey;
+};
